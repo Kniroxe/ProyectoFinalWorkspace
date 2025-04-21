567b06607762ddf7c5423ba8f6efdc92af19aa44
Kniroxe <leo.ooo2008@hotmail.com>
2025-04-21 10:12:40 -0600

    primer commit

---
+++ ECommerceCommos/src/test/java/com/kirbys/ecommerce_commos/ECommerceCommosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirbys.ecommerce_commos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceCommosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
