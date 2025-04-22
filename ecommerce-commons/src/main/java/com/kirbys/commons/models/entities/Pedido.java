35db47dc156027268bbf001effd94f838eb1c9df
cyan-sp <cyan.mv@gmail.com>
2025-04-22 01:45:09 -0600

    feat: agregar modelos y DTOs con sus mappers y servicios
    Añadir entidades principales (Cliente, Estado, Pedido, Producto) al módulo commons
    Crear DTOs, mappers, repositorios e implementaciones de servicios para los microservicios de Cliente y Producto

---
+++ ecommerce-commons/src/main/java/com/kirbys/commons/models/entities/Pedido.java
@@ -0,0 +1,55 @@
+package com.kirbys.commons.models.entities;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PEDIDOS")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PEDIDOS")
+    @SequenceGenerator(name="SEQ_PEDIDOS", sequenceName = "SEQ_PEDIDOS", allocationSize = 1)
+    @Column(name="ID_PEDIDO")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="CLIENTE", referencedColumnName = "ID_CLIENTE")
+    private Cliente cliente;
+
+
+
+	/*
+	 * @ManyToMany(cascade = CascadeType.MERGE)
+	@JoinTable(
+			name = "POKEMON_TIPO",
+			joinColumns = @JoinColumn(name = "ID_POKEMON"),
+			inverseJoinColumns = @JoinColumn(name = "ID_TIPO")
+			)
+	private Set<Tipo> tipos;
+	*/
+
+    @Column(name = "TOTAL")
+    private Long total;
+
+    @Column(name = "FECHA_CREACION ")
+    private LocalDate fechaCreacion;
+
+    @Column(name = "ID_ESTADO")
+    private Long estado;
+
+
+
+
+
+}
--- microservicio-productos/src/main/java/com/kirbys/productos/models/repositories/ProductoRepository.java
+++ microservicio-productos/src/main/java/com/kirbys/productos/models/repositories/ProductoRepository.java
@@ -1,8 +1,11 @@
 package com.kirbys.productos.models.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.kirbys.commons.models.entities.Producto;
+
 @Repository
-public class ProductoRepository extends JpaRepository<Producto, Long>{
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
 
 }
